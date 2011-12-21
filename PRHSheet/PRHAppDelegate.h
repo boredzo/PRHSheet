0451c37469c327023cf59ef627b6e2fef114f70f
Peter Hosey <prh@Featherweight.local>
2011-12-20 22:38:41 -0800

    Initial Commit

---
+++ PRHSheet/PRHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRHAppDelegate.h
+//  PRHSheet
+//
+//  Created by Peter Hosey on 2011-12-20.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PRHAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PRHSheet/PRHAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  PRHAppDelegate.m
+//  PRHSheet
+//
+//  Created by Peter Hosey on 2011-12-20.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PRHAppDelegate.h"
+
+@implementation PRHAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
